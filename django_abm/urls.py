7228e80d12159adfc62ccceff8401eb8147c60e4
adamantike <adamantike@users.noreply.github.com>
2014-10-26 19:25:09 -0300

    Corrección de importación del archivo de configuración

--- django_abm/urls.py
+++ django_abm/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 from django.views.defaults import *
-import settings
+
+from .settings import MEDIA_ROOT
 
 
 urlpatterns = patterns('',
@@ -12,7 +13,7 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     
-    url(r'^media/(?P<path>.*)$','django.views.static.serve',{'document_root': settings.MEDIA_ROOT}),
+    url(r'^media/(?P<path>.*)$','django.views.static.serve',{'document_root': MEDIA_ROOT}),
     
     url(r'^', include('apps.home.urls', namespace='home', app_name='home')),
     url(r'^persona/', include('apps.persona.urls', namespace='persona', app_name='persona')),
